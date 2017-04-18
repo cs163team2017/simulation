597306cff0e8c993fb2b4bbaf5d4dc4f35d215b4
matthew-p <matthewpische@gmail.com>
2017-04-17 22:36:43 -0400

    fix special needs

--- src/Simulation/LimitedTimePerson.java
+++ src/Simulation/LimitedTimePerson.java
@@ -1,12 +1,20 @@
 package Simulation;
 
 public class LimitedTimePerson extends Person {
+
     /** modifier to increase time this person spends at cashier */
     private final double CASHIER_TIME_MODIFIER = 1;
     /** modifier to increase the time until this person exits the sim */
     private final double LEAVE_TIME_MODIFIER = 0.5;
     /** modifier to increase time this person spends at the eatery */
     private final double EATERY_TIME_MODIFIER = 0.5;
+    
+    public LimitedTimePerson(double eateryTime, double leaveTime,
+            double cashierTime, int creationTime) {
+        super(eateryTime, leaveTime, cashierTime, creationTime);
+        // TODO Auto-generated constructor stub
+    }
+
     
     @Override
     public void setEateryTime(double time) {
